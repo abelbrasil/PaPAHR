bde8413c8cf1858a98fb52a2324ec248d2919f96
Gustavo Braga <gustaa1320@gmail.com>
2024-07-17 16:46:58 -0300

    Criando a função get_datasus_from_local

--- R/6_1_get_datasus_from_local.R
+++ R/6_1_get_datasus_from_local.R
@@ -1,40 +1,27 @@
 
-#' get_datasus: Chamar e executar todas as outras funções definidas no pacote.
+#' get_datasus_from_local: Chamar e executar todas as outras funções definidas no pacote.
 #'
-#' @param year_start Um numero de 4 digitos, indicando o ano de inicio para o download dos dados.
-#' @param month_start Um numero de 2 digitos, indicando o mes de inicio para o download dos dados.
-#' @param year_end Um numero de 4 digitos, indicando o ano de termino para o download dos dados.
-#' @param month_end Um numero de 2 digitos, indicando o mes de termino para o download dos dados.
 #' @param state_abbr String. Sigla da Unidade Federativa
 #' @param county_id Codigo(s) do Municipio de Atendimento
+#' @param dbc_dir_path Caminho para o diretorio dos arquivos .dbc
 #' @param information_system String. Valores aceitos "SIA", "SIH-AIH", "SIH-SP"
 #' @param health_establishment_id COdigo(s) do estabelecimento de saude
 #'
 #' @return output
 #' @export
 #'
-get_datasus <-
-  function(year_start,
-           month_start,
-           year_end,
-           month_end,
-           state_abbr,
+get_datasus_from_local <-
+  function(state_abbr,
            county_id,
+           dbc_dir_path,
            information_system, # "SIA","SIH-AIH","SIH-SP"
-           health_establishment_id){
+           health_establishment_id) {
 
     `%>%` <- dplyr::`%>%`
-    publication_date_start <- lubridate::ym(stringr::str_glue("{year_start}-{month_start}"))
-    publication_date_end <- lubridate::ym(stringr::str_glue("{year_end}-{month_end}"))
-
-    #$ Fase 2: Ler os arquivos SIGTAP
-
-    #$ Arquivo get_procedures_details
 
     procedure_details <- get_procedure_details()
 
     cbo <- get_detail("CBO")
-
     cid <- get_detail("CID") %>%
       dplyr::mutate(
         #NO_CID = iconv(NO_CID, "latin1", "UTF-8"),
@@ -42,19 +29,12 @@ get_datasus <-
         NO_CID = stringr::str_c(CO_CID, NO_CID, sep="-")
       )
 
-    #$ ______________________________________
-
-    #$ Fase 3: baixar os arquivos .dbc PA, RD, RJ e SP
-
-    tmp_dir <- tempdir()
-
-    data_source = stringr::str_sub(information_system, 1, 3)
-
     data_type = switch(information_system,
                        "SIA" = "PA",
                        "SIH-AIH" = c("RD", "RJ"),
                        "SIH-SP" = "SP")
 
+    tmp_dir <- tempdir()
     information_system_dir <- stringr::str_glue("{tmp_dir}\\{information_system}")
     if (!dir.exists(information_system_dir)){
       dir.create(information_system_dir)
@@ -63,32 +43,15 @@ get_datasus <-
       unlink(arquivos,recursive = TRUE)
     }
 
-    base_url <- stringr::str_glue("ftp://ftp.datasus.gov.br/dissemin/publicos/{data_source}SUS/200801_/Dados/")
+    dbf_files <- list.files(dbc_dir_path, pattern = "\\.dbc$", full.names = FALSE)
 
-    connection <- curl::curl(base_url)
-
-    #$ Listar todos os arquivos (PA, RD, e etc) do DATASUS
-    dir_files <-
-      connection %>%
-      readLines() %>%
-      stringr::str_sub(start=-12) %>%
-      tibble::as_tibble_col(column_name = "file_name") %>%
-      dplyr::mutate(
-        state = stringr::str_sub(file_name, 3, 4),
-        publication_date = lubridate::ym(stringr::str_sub(file_name, 5, 8),quiet = TRUE),
-        file_type = stringr::str_sub(file_name, 1, 2)) %>%
-      dplyr::filter(file_type %in% data_type,
-        state == state_abbr,
-        publication_date >= publication_date_start,
-        publication_date <= publication_date_end)
-
-    close(connection)
-
-    if (any(is.na(dir_files$publication_date))) {
-      warning("Algumas datas não puderam ser analisadas corretamente. No argumento: publication_date = lubridate::ym(stringr::str_sub(file_name, 5, 8)).")
+    if(is.vector(data_type)){
+      files_name <- dbf_files[grep(paste0(data_type[1],state_abbr), dbf_files)]
+      files_name <- c(files_name, dbf_files[grep(paste0(data_type[2],state_abbr), dbf_files)])
+    } else if (data_type=='PA' || data_type=='SP') {
+      files_name <- dbf_files[grep(paste0(data_type,state_abbr), dbf_files)]
     }
 
-    files_name <- dplyr::pull(dir_files, file_name)
     n_files <- files_name %>% length()
     chunk_size <- ceiling(0.2*n_files)
     n_chunks <- ceiling(n_files/chunk_size)
@@ -103,12 +66,9 @@ get_datasus <-
       chunk = chunk + 1
     }
 
-    for (chunk in 1:n_chunks) {
 
-      dir.create(stringr::str_glue("{tmp_dir}\\{information_system}\\chunk_{chunk}"))
-      download_files_url <- stringr::str_glue("{base_url}{files_chunks[[chunk]]}")
-      output_files_path <- stringr::str_glue("{tmp_dir}\\{information_system}\\{names(files_chunks)[chunk]}\\{files_chunks[[chunk]]}")
-      purrr::walk2(download_files_url, output_files_path, curl::curl_download)
+    for (chunk in 1:n_chunks) {
+      output_files_path <- stringr::str_glue("{dbc_dir_path}\\{files_chunks[[chunk]]}")
 
       if (information_system == "SIA") {
 
@@ -151,7 +111,7 @@ get_datasus <-
           stringr::str_sub(start=-12) %>%
           tibble::as_tibble_col(column_name = "file_name") %>%
           dplyr::mutate(file_type = stringr::str_sub(file_name, 1, 2),
-                 file_id = as.character(dplyr::row_number()))
+                        file_id = as.character(dplyr::row_number()))
         dplyr::select(file_type, file_id)
 
         raw_SIH <- purrr::map_dfr(output_files_path, read.dbc::read.dbc, as.is=TRUE, .id="file_id")
@@ -180,12 +140,11 @@ get_datasus <-
         )
       }
 
-      output_path <- stringr::str_glue("{tmp_dir}\\{information_system}\\{names(files_chunks)[chunk]}\\output{information_system}_chunk_{chunk}.rds")
+      output_path <- stringr::str_glue("{tmp_dir}\\{information_system}\\output{information_system}_chunk_{chunk}.rds")
 
       saveRDS(output,
               file = output_path)
     }
-
     output <-
       tempdir() %>%
       list.files(information_system, full.names=TRUE, recursive=TRUE) %>%
@@ -193,7 +152,4 @@ get_datasus <-
       purrr::map_dfr(readRDS)
 
     return(output)
-
   }
-
-
--- R/7_create_output.R
+++ R/7_create_output.R
@@ -1,137 +1,105 @@
 
 #' Create SUS-SIA/SIH database
 #'
-#' @description
-#' [create_output] Processar arquivos dos sistemas de informação SIA e SIH (DATASUS) e combiná-los com informações do CNES e SIGTAP.
+#' @description Processar arquivos dos sistemas de informação SIA e SIH (DATASUS) e combiná-los com informações do CNES e SIGTAP.
 #'
-#' @param year_start Ano de início da realização do procedimento
-#' @param month_start Mês de início da realização do procedimento
-#' @param year_end Ano de término da realização do procedimento
-#' @param month_end Mês de término da realização do procedimento
-#' @param state_abbr Sigla da Unidade Federativa
+#' @param year_start Um numero de 4 digitos, indicando o ano de inicio para o download dos dados.
+#' @param month_start Um numero de 2 digitos, indicando o mes de inicio para o download dos dados.
+#' @param year_end Um numero de 4 digitos, indicando o ano de termino para o download dos dados.
+#' @param month_end Um numero de 2 digitos, indicando o mes de termino para o download dos dados.
+#' @param state_abbr String. Sigla da Unidade Federativa
 #' @param county_id Codigo(s) do Municipio de Atendimento
-#' @param health_establishment_id Código(s) do estabelecimento de saúde
+#' @param health_establishment_id COdigo(s) do estabelecimento de saude
 #'
 #' @examples
-#' \dontrun{ dados = create_output(year_start=2023,
-#'                   month_start=1,
-#'                   year_end=2023,
-#'                   month_end=3,
-#'                   state_abbr="CE",
-#'                   county_id="230440",
-#'                   health_establishment_id=c("2561492","2481286")
-#'                   )}
+#'   dados = create_output(
+#'     year_start = 2023,
+#'     month_start = 1,
+#'     year_end = 2023,
+#'     month_end = 3,
+#'     state_abbr = "CE",
+#'     county_id = "230440",
+#'     health_establishment_id = c("2561492", "2481286")
+#'   )
 #' @export
-create_output <- function(year_start, month_start,
-                          year_end, month_end, state_abbr,
-                          county_id="all",
-                          health_establishment_id="all") {
+create_output <-
+  function(year_start,
+           month_start,
+           year_end,
+           month_end,
+           state_abbr,
+           county_id = "all",
+           health_establishment_id = "all") {
 
-  tempo_inicio <- system.time({
-    state_abbr = toupper(trimws(state_abbr))
-    get_counties(state_abbr, county_id)
+    tempo_inicio <- system.time({
+      state_abbr = toupper(trimws(state_abbr))
 
-    download_sigtap_files(
-      year_start,
-      month_start,
-      year_end,
-      month_end,
-      newer = FALSE)
-
-  outputSIA <- get_datasus(
-    year_start,
-    month_start,
-    year_end,
-    month_end,
-    state_abbr,
-    county_id,
-    information_system = "SIA",
-    health_establishment_id
-  )
-
-  outputSIH_AIH <- get_datasus(
-    year_start,
-    month_start,
-    year_end,
-    month_end,
-    state_abbr,
-    county_id,
-    information_system = "SIH-AIH",
-    health_establishment_id
-  )
-
-  outputSIH_SP <- get_datasus(
-    year_start,
-    month_start,
-    year_end,
-    month_end,
-    state_abbr,
-    county_id,
-    information_system = "SIH-SP",
-    health_establishment_id
-   )
-
-  rm("counties", envir = .GlobalEnv)
-  rm("health_establishment", envir = .GlobalEnv)
-
-  # Salva os data frames em arquivos CSV no diretorio atual
-  write.csv2(outputSIA, "./data-raw/outputSIA.csv", na="", row.names=FALSE)
-  write.csv2(outputSIH_AIH, "./data-raw/outputSIH_AIH.csv", na="", row.names=FALSE)
-  write.csv2(outputSIH_SP, "./data-raw/outputSIH_SP.csv", na="", row.names=FALSE)
-  })
-  cat("Tempo de execução:", tempo_inicio[3]/60, "minutos\n")
-  return(list(outputSIA, outputSIH_AIH, outputSIH_SP))
-
-}
-
-#' Create SUS-SIA/SIH database from local DBC files
-#'
-#' @description
-#' [create_output_from_local] preprocess DBC files from SIA and SIH information systems (DATASUS)
-#' and match with CNES and SIGTAP information.
-#'
-
-#' @param dbc_dir_path Diretório que contêm os arquivos DBC
-#' @param health_establishment_id Código(s) do estabelecimento de saúde
-#'
-#' @examples
-#' \dontrun{create_output(tempdir(),
-#'                        health_establishment_id=c("2561492","2481286"),
-#'                        county_id="230440",
-#'                        )}
+      get_counties(state_abbr, county_id)
+      health_establishment = get_CNES()
 
 
-#' @export
-create_output_from_local <- function(dbc_dir_path, health_establishment_id="all", county_id="all") {
-  outputSIA <- get_datasus_from_local(
-    dbc_dir_path,
-    information_system = "SIA",
-    county_id,
-    health_establishment_id
-  )
+      download_sigtap_files(year_start,
+                            month_start,
+                            year_end,
+                            month_end,
+                            newer = FALSE)
 
-  outputSIH_AIH <- get_datasus_from_local(
-    dbc_dir_path,
-    information_system = "SIH-AIH",
-    county_id,
-    health_establishment_id
-  )
+      outputSIA <- get_datasus(
+        year_start,
+        month_start,
+        year_end,
+        month_end,
+        state_abbr,
+        county_id,
+        information_system = "SIA",
+        health_establishment_id
+      )
 
-  outputSIH_SP <- get_datasus_from_local(
-    dbc_dir_path,
-    information_system = "SIH-SP",
-    county_id,
-    health_establishment_id
-  )
+      outputSIH_AIH <- get_datasus(
+        year_start,
+        month_start,
+        year_end,
+        month_end,
+        state_abbr,
+        county_id,
+        information_system = "SIH-AIH",
+        health_establishment_id
+      )
 
-  write.csv2(outputSIA, "outputSIA.csv", na="", row.names=FALSE)
-  write.csv2(outputSIH_AIH, "outputSIH_AIH.csv", na="", row.names=FALSE)
-  write.csv2(outputSIH_AIH, "outputSIH_SP.csv", na="", row.names=FALSE)
+      outputSIH_SP <- get_datasus(
+        year_start,
+        month_start,
+        year_end,
+        month_end,
+        state_abbr,
+        county_id,
+        information_system = "SIH-SP",
+        health_establishment_id
+      )
 
-  return(list(outputSIA, outputSIH_AIH, outputSIH_SP))
-}
+      rm("counties", envir = .GlobalEnv)
+      rm("health_establishment", envir = .GlobalEnv)
 
+      # Salva os data frames em arquivos CSV no diretorio atual
+      write.csv2(outputSIA,
+                 "./data-raw/outputSIA.csv",
+                 na = "",
+                 row.names = FALSE)
+      write.csv2(
+        outputSIH_AIH,
+        "./data-raw/outputSIH_AIH.csv",
+        na = "",
+        row.names = FALSE
+      )
+      write.csv2(
+        outputSIH_SP,
+        "./data-raw/outputSIH_SP.csv",
+        na = "",
+        row.names = FALSE
+      )
+    })
+    cat("Tempo de execução:", tempo_inicio[3]/60,"minutos\n")
+    return(list(outputSIA, outputSIH_AIH, outputSIH_SP))
 
-
-
+  }
 
